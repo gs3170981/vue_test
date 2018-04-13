c4190bde1d658277c594b70e19c7ec23bac2b4d8
guosheng <1535233734@qq.com>
2018-04-13 18:01:32 +0800

    【底层】上传

--- js/vue_test.js
+++ js/vue_test.js
@@ -7,37 +7,107 @@ methods*/
 const T = {
   container: document.createDocumentFragment(),
   'v-for' (obj) {
-    let row = document.createDocumentFragment()
-    let child = obj.child
+//  let row = document.createDocumentFragment()
+//  let child = obj.child
     let f_arr = obj.val.split(' ')
-    
-    
+
+    let r_obj = {}
+    r_obj[f_arr[0]] = obj.data[f_arr[f_arr.length - 1]]
 //  for (let i = 0; i < child.length; i++) {
 //    row.appendChild()
 //  }
 //  let p = document.createElement("p");
+//  console.log(obj)
+    return r_obj
+  },
+  'v-test' (obj) {
+    let val = obj.val // 这里只处理了一层，如遇到t.a[0].asd  之类的再做处理
+
+    let t = val.substring(0, val.indexOf('.'))
+    let _obj = val.substring(val.lastIndexOf('.') + 1, val.length)
+    let data = obj.data[t]
+    let r_obj = []
+    for (let i = 0; i < data.length; i++) {
+      r_obj
+    }
+    
+    _obj[t] = 
+    return r_obj
+//  console.log(obj)
+    
   }
 }
 const F = {
   $ (dom) {
     return document.getElementById(dom.substring(dom[0] === '#' ? 1 : 0, dom.length))
   },
-  dom_rec (data) {
-    let child = data.el.children
-    for (let i = 0; i < child.length; i++) {
-      let c_attr = child[i].attributes
-      let c_child = child[i].children
+  dom_rec (obj) {
+    /* 思路：先递归到节点最深处,从深处进行回朔处理 */
+    let dom = obj.dom
+    for (let i = 0; i < dom.length; i++) {
+      let child = dom[i].children
+      let c_attr = dom[i].attributes
+      let _obj = {}
       for (let j = 0; j < c_attr.length; j++) {
         let key = c_attr[j].name
         let val = c_attr[j].value
-        T[key] && T[key]({
+        _obj = T[key] && T[key]({
           val: val,
-          child: c_child,
-          data
+          data: obj.data,
+          source_data: obj.source_data // 有可能从对象顶部执行，所以要传，例如item.xx
         })
       }
+      if (child) {
+        this.dom_rec({
+          dom: child,
+          data: _obj,
+          source_data: obj.source_data // 有可能从对象顶部执行，所以要传，例如item.xx
+        })
+      }
+      console.log(_obj)
+      
+      
+      
+//    let c_attr = dom[i].attributes
+//    for (let j = 0; j < c_attr.length; j++) {
+//      let key = c_attr[j].name
+//      let val = c_attr[j].value
+//      T[key] && T[key]({
+//        val: val,
+//        child: c_attr[j],
+//      })
+//    }
+//    console.log(c_attr)
     }
+    
+    
+//  let child = obj.dom.children
+//  if (child) {
+//    for (let i = 0; i < child.length; i++) {
+//      this.dom_rec({
+//        dom: child
+//      })
+//    }
+//  }
+//  let c_attr = child[i].attributes
+    
+    
+    
+//  for (let i = 0; i < child.length; i++) {
+//    let c_attr = child[i].attributes
+//    let c_child = child[i].children
+//    for (let j = 0; j < c_attr.length; j++) {
+//      let key = c_attr[j].name
+//      let val = c_attr[j].value
+//      T[key] && T[key]({
+//        val: val,
+//        child: c_child,
+//        data
+//      })
+//    }
+//  }
   }
+  
 }
 
 class vue_test {
@@ -52,7 +122,13 @@ class vue_test {
     this.init()
   }
   init () {
-    let el_json = F.dom_rec(this.data)
+    let data = this.data.data
+    let child = this.data.el.children
+    let el_json = F.dom_rec({
+      dom: child, // id所在的根目录不执行
+      data: data,
+      source_data: data
+    })
   }
   
   
